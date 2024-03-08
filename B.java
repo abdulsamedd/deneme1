6bc634ef6b92a328c202ee4218e399b3b62d1025
MustfaOzcan <m.ustafa190529@gmail.com>
2024-03-09 01:19:31 +0300

    Create B.java

---
+++ B.java
@@ -0,0 +1,18 @@
+package test;
+
+public class B{
+
+/**
+ * A javadoc comment.
+ * @param args
+ */
+public static deneme(){
+
+    System.out.println("/* This isn't a comment */");
+    /* This is a comment */
+    //Comment
+    System.out.println("//Not comment");
+}
+
+//Comment
+}
